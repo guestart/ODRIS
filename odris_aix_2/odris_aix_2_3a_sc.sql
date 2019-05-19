2536692a1ff83296e767f5381759bb1efd202c2c
Quanwen Zhao <guestart@163.com>
2019-05-19 11:13:46 +0800

    Create odris_aix_2_3a_sc.sql

---
+++ odris_aix_2/odris_aix_2_3a_sc.sql
@@ -0,0 +1,16 @@
+PROMPT =================================
+PROMPT # 3.0 System Configuration (SC) #
+PROMPT =================================
+PROMPT
+PROMPT =============================
+PROMPT # 3.1 Disk utilization rate #
+PROMPT =============================
+PROMPT
+HOST df -g
+--PROMPT
+PROMPT ============================
+PROMPT # 3.2 CPU utilization rate #
+PROMPT ============================
+--PROMPT
+HOST vmstat 2 20
+--PROMPT
