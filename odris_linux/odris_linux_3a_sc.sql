3ee92237ff1e394e992736d10751d3c0942c06be
Quanwen Zhao <guestart@163.com>
2019-05-26 15:05:15 +0800

    Create odris_linux_3a_sc.sql

---
+++ odris_linux/odris_linux_3a_sc.sql
@@ -0,0 +1,14 @@
+PROMPT =================================
+PROMPT # 3.0 System Configuration (SC) #
+PROMPT =================================
+PROMPT
+PROMPT =============================
+PROMPT # 3.1 Disk utilization rate #
+PROMPT =============================
+PROMPT
+HOST df -h
+PROMPT ============================
+PROMPT # 3.2 CPU utilization rate #
+PROMPT ============================
+PROMPT
+HOST vmstat 2 20
