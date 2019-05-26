64d4e789fd219b2f6cdf717cc1d3357f116334b5
Quanwen Zhao <guestart@163.com>
2019-05-26 15:01:05 +0800

    Create odris_linux.sql

---
+++ odris_linux/odris_linux.sql
@@ -0,0 +1,22 @@
+REM
+REM    Script: odris_linux.sql
+REM    Author: Quanwen Zhao
+REM    Updated: May 24, 2019
+REM
+REM    Purpose:
+REM      This sql script "odris" (the first letter abbreviation of oracle database routine inspection script)
+REM      usually checks some info of oracle database on LINUX including server's hardware configuration.
+REM
+
+@@header.sql
+
+@@odris_linux_1a_obi.sql
+@@odris_linux_2a_shc.sql
+@@odris_linux_3a_sc.sql
+@@odris_linux_4a_dc.sql
+@@odris_linux_5a_sm.sql
+@@odris_linux_6a_asd.sql
+@@odris_linux_7a_tid.sql
+@@odris_linux_8a_rb.sql
+
+@@footer.sql
